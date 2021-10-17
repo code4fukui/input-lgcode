a000c342c219707af24532c6c3b101ce502cb95d
Taisuke Fukuno <fukuno@jig.jp>
2021-10-18 06:52:10 +0900

    fix required

--- input-lgcode.js
+++ input-lgcode.js
@@ -1,6 +1,9 @@
 import { SelectCity } from "https://code4fukui.github.io/select-city/select-city.js";
 
 class InputLGCode extends SelectCity {
+  constructor(opts) {
+    super(opts);
+  }
   async init(opts) {
     await super.init(opts);
     this.appendChild(this.inlgcode);
